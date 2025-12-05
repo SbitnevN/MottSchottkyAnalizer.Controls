e65b9a952cb24be44bede86548944881227e287c
Nikita Sbitnev <118511957+SbitnevN@users.noreply.github.com>
2025-12-05 23:11:36 +0700

    Initial commit

---
+++ Files/FileSelector.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace MottSchottkyAnalizer.Controls.Files;
+/// <summary>
+/// Interaction logic for FileSelector.xaml
+/// </summary>
+public partial class FileSelector : UserControl
+{
+    public FileSelector()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Files/Localization/LocalizedStrings.cs
@@ -0,0 +1,7 @@
+﻿namespace MottSchottkyAnalizer.Controls.Files.Localization;
+
+public static class LocalizedStrings
+{
+    public static string Import => "Импорт";
+    public static string SuccessfulImport => "Успешный импорт!";
+}
